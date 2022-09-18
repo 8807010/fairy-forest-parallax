c4045400bf3dd928a2f42960cb9d6e309d51a71a
Artiom_Sokolov <sokolova-net@mail.ru>
2022-09-18 23:51:58 +0300

    parallax for scroll

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-console.log("hello");
+window.addEventListener('scroll', e => {
+  document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`)
+  // document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+});
+
+
